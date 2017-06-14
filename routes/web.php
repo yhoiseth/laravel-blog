98666f19c38898eabe1c352017c99a06a4dc4e6d
Yngve Høiseth <yhoiseth@gmail.com>
2017-06-14 20:23:15 +0200

    Finish episode 7

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,15 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Task;
+
+
+
+
 
 Route::get('/tasks', function () {
     $tasks = DB::table('tasks')->latest()->get();
+
+    $tasks = Task::all();
 
     return view(
         'tasks.index',
@@ -21,7 +18,7 @@ Route::get('/tasks', function () {
 });
 
 Route::get('/tasks/{id}', function ($id) {
-    $task = DB::table('tasks')->find($id);
+    $task = Task::find($id);
 
     return view(
         'tasks.show',
